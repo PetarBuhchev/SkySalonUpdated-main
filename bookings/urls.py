9abaf320d52336a902d572b1ecac6a1e5c1e8b01
PetarBuhchev <petarbuhchev@outlook.com>
2025-10-30 23:20:57 +0200

    30.10 website

---
+++ bookings/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("book/", views.book, name="book"),
+    path("booking-success/", views.booking_success, name="booking_success"),
+    path("pricelist/", views.pricelist, name="pricelist"),
+    path("calendar/", views.calendar_view, name="calendar"),
+    path("workers/<int:worker_id>/", views.worker_detail, name="worker_detail"),
+]
+
+
+
